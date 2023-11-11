a56fc7b70d18f0deb7a0d57ef1707f4538407ec3
OSGERARDO <nmsosx@gmail.com>
2023-11-11 03:43:03 -0300

    ReactApp

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className="bg-dark text-light p-3">
+            <h4 className="text-center">Todos los derechos Reservados</h4>
+        </div >
+    )
+}
