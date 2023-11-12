92bb3bfea9e6e614c855a778ac325c7af30ada09
OSGERARDO <nmsosx@gmail.com>
2023-11-12 02:59:20 -0300

    styles about contact policy

--- client/src/pages/Pagenotfound.jsx
+++ client/src/pages/Pagenotfound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 
 const Pagenotfound = () => {
@@ -6,6 +7,10 @@ const Pagenotfound = () => {
     <Layout>
       <div className="pnf">
         <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Pagina no Encontrada</h2>
+        <Link to="/" className="pnf-btn">
+          Regresar
+        </Link>
       </div>
     </Layout>
   );
