6dbedd223f034ae5d6f35f8dd76d98cbad08af0b
OSGERARDO <nmsosx@gmail.com>
2023-11-13 00:38:27 -0300

    useState

---
+++ client/src/pages/Auth/Register.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //form function
+
+  return (
+    <Layout title="Register - Ecommerce App">
+      <div className="register">
+        <h1>Pagina de Registro</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Ingresa tu nombre"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Ingresa tu Email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Ingresa tu contraseña"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputPhone1"
+              placeholder="Ingresa tu telefono"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputAddress1"
+              placeholder="Ingresa tu Dirección"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Registrarse
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -7,19 +7,19 @@ const Contact = () => {
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
+            src="/images/contactus.png"
             alt="contactus"
             style={{ width: "100%" }}
           />
         </div>
         <div className="col-md-4">
-          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
+          <h1 className="bg-dark p-2 text-white text-center">CONTACTANOS</h1>
           <p className="text-justify mt-2">
             any query and info about prodduct feel free to call anytime we 24X7
             vaialible
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : www.help@ventadegarage.com
           </p>
           <p className="mt-3">
             <BiPhoneCall /> : 012-3456789
