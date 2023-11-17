452ba1be03e05700bf7ddc42e98332f0c0a1d761
OSGERARDO <nmsosx@gmail.com>
2023-11-16 21:49:53 -0300

    Products API CRUD

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoutes.js'
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 import cors from 'cors'
+import { getSingleProductController } from './controllers/productController.js';
 
 //configurar env
 dotenv.config();
@@ -24,9 +26,8 @@ app.use(morgan('dev'));
 //routes
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-
-
-
+app.use("/api/v1/product", productRoutes);
+app.use('/api/v1/product/get-product/:slug', getSingleProductController);
 
 //rest api
 app.get("/", (req, res) => {
